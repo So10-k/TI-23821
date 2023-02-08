d6bac2d90f31c3d5390801dbbc8b6f9ed6214b3a
P05h <121305788+P05h@users.noreply.github.com>
2023-02-08 07:22:53 -0500

    Update soundboard.js

--- Scripts/soundboard.js
+++ Scripts/soundboard.js
@@ -11,7 +11,7 @@ let sound_data = [
         path: "Soundboard/AUUGHHH.mp3",
     },
     {
-        name: "Are you sure about that",
+        name: "Are You Sure About That",
         id: "rusureabtdat",
         path: "Soundboard/Are you sure about that.3gp",
     },
